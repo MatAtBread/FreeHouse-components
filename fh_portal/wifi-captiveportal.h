a53f64dd743f10fb3a60dbb6ee2e56ecc9af3f89
Matt <bread@mailed.me.uk>
2026-04-21 21:45:11 +0100

    Initial commit

---
+++ fh_portal/wifi-captiveportal.h
@@ -0,0 +1,15 @@
+#ifndef WIFI_CAPTIVEPORTAL_H
+#define WIFI_CAPTIVEPORTAL_H
+
+#include "esp_http_server.h"
+
+class HttpGetHandler {
+  public:
+    virtual esp_err_t getHandler(httpd_req_t *req) = 0;
+  };
+
+void start_captive_portal(HttpGetHandler *handler, const char *ssid);
+void start_web_server(HttpGetHandler *_handler);
+void stop_web_server(void);
+void stop_captive_portal(void);
+#endif
